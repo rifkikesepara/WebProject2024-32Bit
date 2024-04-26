bfe9ba1db26d9b7da636d764cb04104dea5909fe
rifkikesepara <rifkikesepara7@gmail.com>
2024-04-27 00:03:21 +0200

    Check the Price Fix

--- src/pages/Sale.js
+++ src/pages/Sale.js
@@ -165,7 +165,7 @@ export default function Sale() {
                   fontWeight: "bold",
                 }}
               >
-                {t("checkprice").toUpperCase()}
+                {t("checkPrice").toUpperCase()}
               </Typography>
             </motion.div>
           </IconButton>
